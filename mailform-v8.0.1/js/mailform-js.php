065f463898edc9d4453044b3bd4036adea672001
Liang <92705187+liangfromtaiwan@users.noreply.github.com>
2023-03-15 10:39:56 +0000

    1

--- mailform-v8.0.1/js/mailform-js.php
+++ mailform-v8.0.1/js/mailform-js.php
@@ -4,10 +4,19 @@
 session_start();
 error_reporting( E_ALL );
 
+
+
+
 mb_language( 'uni' );
 mb_internal_encoding( 'UTF-8' );
 
+
+
+
 require_once( dirname( __FILE__ ) .'/class.mailform-js.php' );
 $responsive_mailform_js = new Mailform_Js();
+
+
+
 
 ?>
