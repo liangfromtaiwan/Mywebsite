2d273b546bd39ef3e23fa174b47993ca80e7bacc
Liang <92705187+liangfromtaiwan@users.noreply.github.com>
2023-03-16 20:56:24 +0900

    Update send.php

--- sendmail/send.php
+++ sendmail/send.php
@@ -3,26 +3,26 @@
 use PHPMailer/PHPMailer/PHPMailer;
 use PHPMailer/PHPMailer/Exception;
 
-require'phpmaile/src/Exceptionh.php'
+require'phpmaile/src/Exception.php'
 require'phpmaile/src/PHPMailer.php'
 require'phpmaile/src/SMTP.php'
 
-if(isset()$_POST["send"])){
+if(isset($_POST["send"])){
     $mail = new PHPMailer(true);
 
     $mail ->isSMTP();
     $mail -> Host = 'smtp.gmail.com';
     $mail -> SMTPAuth = true;
-    $mail -> Username = 'sscomplexryan@gmail.com';
+    $mail -> Username = ('aaagoodaaa@gmail.com';)
     $mail -> Password = 'bfycwlzqwbiidyso';
     $mail -> SMTPSecure = 'ssl';
     $mail -> Port = 465;
-    $mail -> setFrom('sscomplexryan@gmail.com') ;
+    $mail -> setFrom('aaagoodaaa@gmail.com') ;
     $mail -> addAddress($_POST["email"]);
-    $mail -> isHTML(ture);
+    $mail -> isHTML(true);
 
-    $mail -> Subjest = $_POST["subject"]
-    $mail -> Body = $_POST["message"]
+    $mail -> Subject = $_POST["subject"];
+    $mail -> Body = $_POST["message"];
     $mail -> send();
 
 echo
