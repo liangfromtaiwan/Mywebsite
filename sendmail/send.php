4e97f2cf1a3c45bef69b9cb0f26f9d1158908c10
Liang <92705187+liangfromtaiwan@users.noreply.github.com>
2023-03-16 21:39:58 +0900

    Update send.php

--- sendmail/send.php
+++ sendmail/send.php
@@ -1,38 +1,39 @@
 <?php
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require'phpmaile/src/Exception.php'
-require'phpmaile/src/PHPMailer.php'
-require'phpmaile/src/SMTP.php'
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
 if(isset($_POST["send"])){
-    $mail = new PHPMailer(true);
+  $mail = new PHPMailer(true);
 
-    $mail ->isSMTP();
-    $mail -> Host = 'smtp.gmail.com';
-    $mail -> SMTPAuth = true;
-    $mail -> Username = ('aaagoodaaa@gmail.com');
-    $mail -> Password = 'qytxdtyeibiyvfuf';
-    $mail -> SMTPSecure = 'ssl';
-    $mail -> Port = 465;
-    $mail -> setFrom('aaagoodaaa@gmail.com') ;
-    $mail -> addAddress($_POST["email"]);
-    $mail -> isHTML(true);
+  $mail->isSMTP();
+  $mail->Host = 'smtp.gmail.com';
+  $mail->SMTPAuth = true;
+  $mail->Username = 'aaagoodaaa@gmail.com'; // Your gmail
+  $mail->Password = 'qytxdtyeibiyvfuf'; // Your gmail app password
+  $mail->SMTPSecure = 'ssl';
+  $mail->Port = 465;
 
-    $mail -> Subject = $_POST["subject"];
-    $mail -> Body = $_POST["message"];
-    $mail -> send();
+  $mail->setFrom('aaagoodaaa@gmail.com'); // Your gmail
 
-echo
-"
-<script>
-alert('Sent Successfully');
-document.location.href='index.html';
-<script>
-";
+  $mail->addAddress($_POST["email"]);
 
+  $mail->isHTML(true);
+
+  $mail->Subject = $_POST["subject"];
+  $mail->Body = $_POST["message"];
+
+  $mail->send();
+
+  echo
+  "
+  <script>
+  alert('Sent Successfully');
+  document.location.href = 'index.php';
+  </script>
+  ";
 }
-
 ?>
